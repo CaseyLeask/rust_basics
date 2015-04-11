e0434e44fd66c67acd49ce1699affdab685100a7
Casey Leask <caseyleask@gmail.com>
2015-04-11 22:17:51 +1000

    Added type example

---
+++ type.rs
@@ -0,0 +1,10 @@
+fn main() {
+    // Type annotated variable
+    let _a_float: f64 = 1.0;
+
+    // This variable is an `i32`
+    let mut _an_integer = 5i32;
+
+    // Error! The type of a variable can't be changed
+    // an_integer = true;
+}
